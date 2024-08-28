7437781a9db2ffd5a0e79ef0b21110e3f516721e
Marco Antônio <marco.aq7@gmail.com>
2024-08-28 01:40:30 -0300

    up module

--- siga.js
+++ siga.js
@@ -1,13 +1,18 @@
-export const SIGA = {
-  username: "",
-  currentIgreja: null,
-  cookie: "",
-  loading: false,
-  error: null,
-  igrejas: [],
-  fluxos: [],
-  eventos: [],
-  competencias: [],
-}
 
-export default SIGA
+(function (global) {
+  // Define o objeto SIGA
+  const SIGA = {
+    username: "",
+    currentIgreja: null,
+    cookie: "",
+    loading: false,
+    error: null,
+    igrejas: [],
+    fluxos: [],
+    eventos: [],
+    competencias: [],
+
+  };
+  // Exponha o SIGA globalmente
+  global.SIGA = SIGA;
+})(window);
