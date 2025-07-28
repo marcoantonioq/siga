c2f5140a48414cda2d8e8bcc9869c2e95cff6b7c
Marco Antônio <marco.aq7@gmail.com>
2025-07-28 19:40:32 -0300

    up

--- start.sh
+++ start.sh
@@ -11,12 +11,12 @@ git fetch --all
 git pull
 
 # Cria rede Docker se não existir
-if ! docker network inspect dockers >/dev/null 2>&1; then
-  echo "🌐 Criando rede Docker: dockers"
-  docker network create --driver bridge dockers
-else
-  echo "✅ Rede Docker 'dockers' já existe."
-fi
+# if ! docker network inspect dockers >/dev/null 2>&1; then
+#   echo "🌐 Criando rede Docker: dockers"
+#   docker network create --driver bridge dockers
+# else
+#   echo "✅ Rede Docker 'dockers' já existe."
+# fi
 
 # Instala dependências se necessário
 echo "📦 Instalando dependências..."
