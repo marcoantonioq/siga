f05f391f52e9880afa7e7a562af2c6eac746d3e9
Marco Antônio <marco.aq7@gmail.com>
2025-08-21 19:14:14 -0300

    up

--- src/infra/express/app/siga/preventiva.js
+++ src/infra/express/app/siga/preventiva.js
@@ -2,52 +2,32 @@ import ky from "ky";
 
 const request = ky.create({ retry: { limit: 5 }, timeout: 60000 });
 
-export async function preventiva({auth, _igreja, date1, date2}) {
+export async function preventiva({ auth, date1, date2 }) {
+  console.log("Dados para processar: ", auth.token.length, date1, date2);
+  
   try {
-    let pagina = 0;
-    const tamanho = 100; 
-    const todos = [];
-
-    while (true) {
-      const response = await request.post(
-        "https://siga-api.congregacao.org.br/api/mnt/mnt003/dados/tabela",
-        {
-          json: {
-            filtro: {
-              codigoTipoChecklist: null,
-              codigoEstabelecimento: null,
-              dataIni: date1,
-              dataFim: date2,
-              status: [0, 3],
-              pesquisaRapida: "",
-            },
-            paginacao: {
-              paginaAtual: pagina,
-              quantidadePorPagina: tamanho,
-              ordenarPor: null,
-              ordenarDirecao: null,
-            },
+    const response = await request.post(
+      "https://siga-api.congregacao.org.br/api/mnt/mnt003/dados/tabela",
+      {
+        json: {
+          filtro: {
+            codigoTipoChecklist: null,
+            codigoEstabelecimento: null,
+            dataIni: date1,
+            dataFim: date2,
+            status: [0, 3],
+            pesquisaRapida: "",
           },
-          headers: {
-            Authorization: `Bearer ${auth.token}`,
-            "Content-Type": "application/json",
-            Accept: "application/json, text/plain, */*",
-          },
-        }
-      ).json();
-
-      const registros = response.dados || [];
-
-      if (!registros.length) break; 
-      todos.push(...registros);
-
-      console.log(
-        `Página ${pagina} carregada com ${registros.length} registros`
-      );
-
-      if (registros.length < tamanho) break; 
-      pagina++;
-    }
+          paginacao: null,
+        },
+        headers: {
+          Authorization: `Bearer ${auth.token}`,
+          "Content-Type": "application/json",
+          Accept: "application/json, text/plain, */*",
+        },
+      }
+    ).json();
+    const todos = response.dados || [];
     console.log(`\nTotal preventiva coletado: ${todos.length} registros\n`);
     return todos;
   } catch (error) {
