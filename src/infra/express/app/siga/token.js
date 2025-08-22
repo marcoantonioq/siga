1cb1ebee3b0b94d8e1433fe338fee61f79f7a626
Marco Antônio <marco.aq7@gmail.com>
2025-08-22 09:36:00 -0300

    up

--- src/infra/express/app/siga/token.js
+++ src/infra/express/app/siga/token.js
@@ -1,30 +1,36 @@
-import ky from "ky";
+import ky from 'ky';
 
 const request = ky.create({ retry: { limit: 5 }, timeout: 60000 });
 
 export async function token({ cookies }) {
   try {
-    const html = await request.get(
-      "https://siga.congregacao.org.br/page.aspx?loadPage=/SIS/SIS99908.aspx?f_inicio=S",
-      {
-        headers: {
-          Cookie: cookies,
-        },
-      }
-    ).text();
+    const html = await request
+      .get(
+        'https://siga.congregacao.org.br/page.aspx?loadPage=/SIS/SIS99908.aspx?f_inicio=S',
+        {
+          headers: {
+            Cookie: cookies,
+          },
+        }
+      )
+      .text();
 
-    const regex = /window\.localStorage\.setItem\("ccbsiga-token-api", '(.+?)'\);/s;
+    const regex =
+      /window\.localStorage\.setItem\("ccbsiga-token-api", '(.+?)'\);/s;
     const match = html.match(regex);
 
     if (match && match[1]) {
       const token = match[1];
+      console.log('Cookies recebidos:', cookies);
+      console.log('Token encontrado:', token);
+
       return token;
     } else {
-      console.log("Token não encontrado.");
+      console.log('Token não encontrado.');
       return null;
     }
   } catch (error) {
-    console.error("Erro na requisição preventiva:", error.message);
+    console.error('Erro na requisição preventiva:', error.message);
     return null;
   }
 }
